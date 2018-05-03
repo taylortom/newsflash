dff9ab56bb779bb3e0f6a239b877204be212d4f8
Tom Taylor <hello@tomtaylor.name>
2018-05-03 12:43:06 +0100

    Refactor to save lines

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import App from './components/app.vue';
 import Vue from 'vue';
 
-new Vue({
-  data: { test: true },
-  render: h => h(App)
-}).$mount('#app');
+new Vue({ render: h => h(App) }).$mount('#app');
