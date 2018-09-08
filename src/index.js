bb63d7e4ead34dd494027766ceea2dcdd9454379
Tom Taylor <tom@taylortom.co.uk>
2018-09-08 20:14:17 +0100

    Hook up FA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import config from './config';
+import FALibrary from './faLibrary';
+
+FALibrary.load(config.get('icons'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
